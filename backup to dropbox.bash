9d0c08d8d06709e7d2c4889ad9829c2a4dfcf54a
Patrick Felstead <patrickfelstead@gmail.com>
2021-12-30 10:25:42 +1030

    modify backup script

--- backup to dropbox.bash
+++ backup to dropbox.bash
@@ -2,5 +2,5 @@
 # Ensure make executable with chmod +x myscript.bash
 # Command backs up the folder to dropbox, excluding the ".pio" hidden directory which contains "libdeps" (library dependencies) and "build" (compiled binaries)
 # Use --dry-run for a preview of what rsync will do
-# rsync --verbose --archive --update --dry-run --exclude '.pio' --exclude '.DS_Store' "$PWD" "/Users/patrickfelstead/Dropbox/Patrick's Files/Electronic Design/PlatformIO"
-rsync --verbose --archive --update --exclude '.pio' --exclude '.DS_Store' "$PWD" "/Users/patrickfelstead/Dropbox/Patrick's Files/Electronic Design/PlatformIO"
+# rsync --dry-run --verbose --archive --update --include 'platformio.ini' --exclude '.pio/build' --exclude 'M5GFX' --exclude 'M5Unified' --exclude 'TFT_eSPI' --exclude '.DS_Store' "$PWD" "/Users/patrickfelstead/Dropbox/Patrick's Files/Electronic Design/PlatformIO"
+rsync --verbose --archive --update --include 'platformio.ini' --exclude '.pio/build' --exclude 'M5GFX' --exclude 'M5Unified' --exclude 'TFT_eSPI' --exclude '.DS_Store' "$PWD" "/Users/patrickfelstead/Dropbox/Patrick's Files/Electronic Design/PlatformIO"
